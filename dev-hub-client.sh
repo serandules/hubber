bf5cdd9857bda4f8a46f280974c99bd98eed804b
ruchiraw <ruchira.wageesha@gmail.com>
2014-10-26 15:33:00 +0530

    updating

---
+++ dev-hub-client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export NODE_ENV=development
+
+#hub-client
+export HUB=false
+clear
+node index.js
---
+++ dev-hub.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export NODE_ENV=development
+
+#hub
+export HUB=true
+clear
+node index.js
