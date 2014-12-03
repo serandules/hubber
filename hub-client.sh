d81d64e4b9212a66b9b07c9d6833f59e2ed15981
ruchiraw <ruchira.wageesha@gmail.com>
2014-12-03 07:42:57 +0530

    updating

--- hub-client.sh
+++ hub-client.sh
@@ -4,6 +4,8 @@ export NODE_ENV=production
 npm install
 
 #hub-client
-export HUB_DIR=~/serandives/hub-client
+export HUB_DIR=/opt/serandives/hub-client
+export DRONES_DIR=/opt/serandives/drones
+export LOGS_DIR=/opt/serandives/logs
 export HUB=false
 node index.js > hub-client.log &
--- hub.sh
+++ hub.sh
@@ -6,6 +6,7 @@ export NODE_ENV=production
 npm install
 
 #hub
-export HUB_DIR=~/serandives/hub
+export HUB_DIR=/opt/serandives/hub
+export LOGS_DIR=/opt/serandives/logs
 export HUB=true
 node index.js > hub.log &
