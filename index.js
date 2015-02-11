20c2a5fd4e91152f2796555554d00774bcdb61a4
ruchiraw <ruchira.wageesha@gmail.com>
2015-02-11 23:16:55 +0530

    updating

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var log = require('logger')('hubber');
 var hubber = require('./lib/hubber');
 
 var hub = process.env.HUB === 'true';
-//hub = true;
 var mode = hub ? 'hub' : 'hub-client';
-log.info('hubber mode : %s', mode);
 
 var REPO = hub ? 'https://github.com/serandules/hub.git' : 'https://github.com/serandules/hub-client.git';
+
+log.info('hubber mode : %s', mode);
 
 hubber.start(REPO, 'index.js', function (err, id, pid, address) {
     if (err) {
