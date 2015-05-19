1660732b107bfbb387d800e1812d88412da6bea4
ruchiraw <ruchira.wageesha@gmail.com>
2015-05-19 23:07:40 +0530

    updating

--- client-dev.sh
+++ client-dev.sh
@@ -4,5 +4,6 @@ export SU_PASS=ruchira
 export NODE_TLS_REJECT_UNAUTHORIZED="0"
 export LOCAL_REPO=/Users/ruchira/sources/git
 export DRONES_DIR=/tmp/serandives/drones
+export NODE_PATH=/Users/ruchira/softwares/node-v0.12.0-darwin-x64/lib/node_modules
 
 node index.js
--- hub-dev.sh
+++ hub-dev.sh
@@ -3,6 +3,7 @@
 export HUB=true
 export LOCAL_REPO=/Users/ruchira/sources/git
 export DRONES_DIR=/tmp/serandives/drones
+export NODE_PATH=/Users/ruchira/softwares/node-v0.12.0-darwin-x64/lib/node_modules
 
 rm -rf $DRONES_DIR/*
 
