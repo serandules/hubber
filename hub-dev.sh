02a1371588d562c72af8e9e9b985b0dae6a029c9
ruchiraw <ruchira.wageesha@gmail.com>
2015-05-23 14:44:13 +0530

    updating

--- hub-dev.sh
+++ hub-dev.sh
@@ -5,6 +5,6 @@ export LOCAL_REPO=/Users/ruchira/sources/git
 export DRONES_DIR=/tmp/serandives/drones
 export NODE_PATH=/Users/ruchira/softwares/node-v0.12.0-darwin-x64/lib/node_modules
 
-rm -rf $DRONES_DIR/*
+#rm -rf $DRONES_DIR/*
 
 node index.js
