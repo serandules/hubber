b83ff54139f794c242bcbe4c617bd931c4262bfa
ruchiraw <ruchira.wageesha@gmail.com>
2015-05-27 09:26:47 +0530

    updating

---
+++ setup-prod.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#install nodejs
+#install mongodb
+#install git
+
+
+#install make
+
+npm install -g async
+npm install -g bcrypt
+npm install -g body-parser
+npm install -g component
+npm install -g component-builder
+npm install -g component-middleware
+npm install -g component-resolver
+npm install -g css-whitespace
+npm install -g express
+npm install -g formidable
+npm install -g http-proxy
+npm install -g knox
+npm install -g knox-mpu
+npm install -g mongoose
+npm install -g node-uuid
+npm install -g rework
+npm install -g rework-mixins
+npm install -g rimraf
+npm install -g serve-favicon
+npm install -g socket.io
+npm install -g socket.io-client
+npm install -g string-to-js
+
+
+
+
