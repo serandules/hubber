018430dc6bd262fd7de26a884ea2306f2cae8875
ruchiraw <ruchira.wageesha@gmail.com>
2015-05-29 22:45:23 +0530

    updating

--- client-prod.sh
+++ client-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export PRODUCTION=true
-export SU_PASS=ruchira
 export NODE_TLS_REJECT_UNAUTHORIZED="0"
 export DRONES_DIR=/tmp/serandives/drones
 export NODE_PATH=$NODE_HOME/lib/node_modules
+export PATH=$NODE_HOME/bin:$PATH
 
-node index.js
+nohup node index.js > hub-client.log &
--- hub-prod.sh
+++ hub-prod.sh
@@ -6,13 +6,6 @@ export DRONES_DIR=/tmp/serandives/drones
 export NODE_PATH=$NODE_HOME/lib/node_modules
 export PATH=$NODE_HOME/bin:$PATH
 
-if [ $HUB = "true" ]
-then
-    LOG="hub.log"
-else
-    LOG="hub-client.log"
-fi
-
 #rm -rf $DRONES_DIR/*
 
-nohup node index.js > $LOG &
+nohup node index.js > hub.log &
