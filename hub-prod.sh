8156ea691a102276c8639be021e8591dceded5a8
ruchiraw <ruchira.wageesha@gmail.com>
2015-05-29 18:31:50 +0530

    updating

--- hub-prod.sh
+++ hub-prod.sh
@@ -6,6 +6,13 @@ export DRONES_DIR=/tmp/serandives/drones
 export NODE_PATH=$NODE_HOME/lib/node_modules
 export PATH=$NODE_HOME/bin:$PATH
 
+if [ $HUB = "true" ]
+then
+    LOG="hub.log"
+else
+    LOG="hub-client.log"
+fi
+
 #rm -rf $DRONES_DIR/*
 
-node index.js &
+nohup node index.js > $LOG &
