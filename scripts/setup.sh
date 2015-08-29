026fe8e6bea8c3d49d2ba2646a8b2c979991406a
serandules <serandules@gmail.com>
2015-08-29 16:59:30 +0530

    updating

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 for dir in serandomps/* ; do
 cd $dir;
-git config --global user.email "serandomps@gmail.com"
-git config --global user.name "serandomps"
+pwd
+git config user.email "serandomps@gmail.com"
+git config user.name "serandomps"
 cd ../..;
 done
 
 for dir in serandules/* ; do
 cd $dir;
-git config --global user.email "serandules@gmail.com"
-git config --global user.name "serandules"
+pwd
+git config user.email "serandules@gmail.com"
+git config user.name "serandules"
 cd ../..;
 done
