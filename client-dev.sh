3d0273ea209bf3d259152def6858bc880d0549db
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-02-21 23:18:15 +0530

    sso improvements

--- client-dev.sh
+++ client-dev.sh
@@ -2,8 +2,8 @@
 
 export SU_PASS=ruchira
 export NODE_TLS_REJECT_UNAUTHORIZED="0"
-# export LOCAL_REPO=/home/ruchiraw/sources/serandives
-export DRONES_DIR=/tmp/serandives/drones
+export LOCAL_REPO=/Users/ruchira/sources/serandives
+export DRONES_DIR=/Users/ruchira/sources/serandives/drones
 export PATH=$NODE_HOME/bin:$PATH
 export NODE_PATH=$NODE_HOME/lib/node_modules
 
